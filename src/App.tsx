457c350320800a73cf70a7d38121d7a6d9dca5de
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-04-23 17:34:00 +0100

    initial project set up and commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className="main"></div>
+    </main>
+  );
+};
+
+export default App;
