19992764b0fbdb6b5f749d2abe6e0921685d464c
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-04-30 17:35:26 +0100

    set up redux for the summary ai api call

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,14 +1,32 @@
 import { useState, useEffect } from 'react';
 import link from '../assets/link.svg';
-import submit from '../assets/submit.svg';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={link}
@@ -18,8 +36,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL to summarize"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-12 pr-12 text-sm shadow-lg font-satoshi font-medium focus:border-black focus:outline-none focus:ring-0 peer"
           />
