1efe947c18896280f959b165142b870ef5dfd3b1
Norman-Ben <90580315+Norman-Ben@users.noreply.github.com>
2023-05-17 16:20:56 +0100

    add react router and create login + register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import Hero from './components/Hero';
 import Demo from './components/Demo';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 import './App.css';
 
 const App = () => {
   return (
-    <main>
-      <div className="main">
-        <div className="gradient" />
-      </div>
-
-      <div className="app">
-        <Hero />
-        <Demo />
-      </div>
-    </main>
+    <Router>
+      <main>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </main>
+    </Router>
   );
 };
 
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero';
+import Demo from '../components/Demo';
+
+function Dashboard() {
+  return (
+    <>
+      <Hero />
+      <Demo />
+    </>
+  );
+}
+
+export default Dashboard;
